46279b04258b70d2c54f404f26facdc5406b564d
李骏 <lj852487887@gmail.com>
2016-12-05 18:34:44 +0800

    update

--- code/elasticsearch/cluster.sh
+++ code/elasticsearch/cluster.sh
@@ -1,4 +1,8 @@
 #command on the cluster
+#
+#
+#check net port
+netstat -tunlp
 
 #check elastcisearch server
 curl -X GET '10.1.2.3:9200'   
@@ -7,3 +11,6 @@ curl '10.1.2.3:9200/?pretty'
 
 #show all indices of es server
 curl '10.1.2.3:9200/_cat/indices?v'
+
+
+curl -XGET 'http://se003:9200/_cluster/state'  
