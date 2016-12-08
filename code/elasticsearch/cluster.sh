2076cedd37b9cdb8cb1cc6c8865b316cd15189be
李骏 <lj852487887@gmail.com>
2016-12-08 20:54:16 +0800

    update

--- code/elasticsearch/cluster.sh
+++ code/elasticsearch/cluster.sh
@@ -9,12 +9,12 @@ ps aux | grep ssh
 netstat -tunlp
 
 #check elastcisearch server
-curl -X GET '10.1.2.3:9200'   
+curl -XGET '10.1.2.3:9200'   
 
 curl '10.1.2.3:9200/?pretty'
 
 #show all indices of es server
-curl '10.1.2.3:9200/_cat/indices?v'
+curl 'se004:9200/_cat/indices?v'
 
 #check cluster state
 curl -XGET 'http://se003:9200/_cluster/state?pretty'  
